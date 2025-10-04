791dcaff54f21daaa7369ba694ad51e779c09217
Jacalz <jacalz@tutanota.com>
2025-10-04 21:24:30 +0200

    Initial untested proof of concept

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::path::PathBuf;
 /// A tool for tagging Exif metadata to scanned images from film rolls.
 struct Args {
     /// Source directory of files to apply metadata to.
-    src: PathBuf,
+    src: Vec<PathBuf>,
 
     /// Set the ISO film speed used.
     #[arg(short, long)]
@@ -18,20 +18,29 @@ struct Args {
     /// First word is parsed as the camera maker while the rest is set as the camera model.
     #[arg(short, long)]
     camera: Option<String>,
-
-    /// Set the lens model used.
-    #[arg(short, long)]
-    lens: Option<String>,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    if !args.src.exists() {
-        return Err(anyhow!("Source directory does not exist"));
+    if args.src.is_empty() {
+        return Err(anyhow!("No files were provided"));
     }
 
-    let meta = Metadata::new_from_path(&args.src)?;
-    println!("{:?}", meta.get_tag_multiple_strings("Exif.Image.Make")?);
+    for file in &args.src {
+        let meta = Metadata::new_from_path(file)?;
+
+        if let Some(iso) = args.iso {
+            meta.set_tag_numeric("Exif.Photo.ISOSpeedRatings", iso as i32)?;
+        }
+
+        if let Some(camera) = &args.camera {
+            let (make, model) = camera.split_once(' ').unwrap_or_default();
+            meta.set_tag_string("Exif.Image.Make", make)?;
+            meta.set_tag_string("Exif.Image.Model", model)?;
+        }
+
+        meta.save_to_file(file)?;
+    }
 
     Ok(())
 }
